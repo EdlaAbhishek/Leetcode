1ffacf7b8dcee3b9788b5132869f5e1f80f82ae0
Abhishek Edla <130242419+EdlaAbhishek@users.noreply.github.com>
2026-07-28 14:16:48 +0530

    Time: 6 ms (22.22%), Space: 46.2 MB (6.23%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int rev=0;
-        int rem=0;
-        double real =x;
-        while(x>0){
-            rem=x%10;
-            rev=rev*10+rem;
-            x=x/10;
+    String number = String.valueOf(x);
+    int left=0,right=number.length()-1;
+    while(left<=right)
+    {
+        if(number.charAt(left)==number.charAt(right))
+        {
+            right--;
+            left++;
         }
-        if(rev==real){
-            return true;
-        }
-        else 
-            return false;
+        else
+        return false;
+    }
+    return true;    
     }
 }
